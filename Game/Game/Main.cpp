367e3a0093f258308ef3f0ffe5ae5be6d4f88c7a
Leapt-Takuya <takuya01@feel.ocn.ne.jp>
2016-06-20 20:21:46 +0900

    [update] コメントを追加

--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -4,11 +4,24 @@ using namespace std;
 
 void printString(string);
 
+/**
+ * [sampleFunction description]
+ * @param  a [description]
+ * @param  b [description]
+ * @return   [description]
+ */
+
 int sampleFunction(int a, int b)
 {
 	return a + b;
 }
 
+/**
+ * [extraFunction description]
+ * @param  a [description]
+ * @param  b [description]
+ * @return   [description]
+ */
 double extraFunction(double a, double b)
 {
 	return (a > b) ? a : b;

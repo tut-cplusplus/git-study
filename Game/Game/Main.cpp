b97708ce7b91b1742ef6f2bd32a73cfee68eb86d
Leapt-Takuya <takuya01@feel.ocn.ne.jp>
2016-06-20 20:06:00 +0900

    [add] 関数の追加

--- Game/Game/Main.cpp
+++ Game/Game/Main.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 void printString(string);
+
+int sampleFunction(int a, int b)
+{
+	return a + b;
+}
+
+double extraFunction(double a, double b)
+{
+	return (a > b) ? a : b;
+}
+
 //コメント
 int main(int argc, char** argv)
 {
